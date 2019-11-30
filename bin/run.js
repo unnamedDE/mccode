aef7515e7e461d281a2d581a6d62a09238f3c4fe
unnamedDE <unnamedde.mail@googlemail.com>
2019-11-30 22:49:13 +0100

    Fixed path

--- bin/run.js
+++ bin/run.js
@@ -12,11 +12,11 @@ args.splice(0, 1);
 
 switch (subcommand) {
   case "compile":
-    index.compile(args, {fullError: flags.some(e => e === "-fullErr"), generateJSON: flags.some(e => e === "-generateJSON"), noErrors: flags.some(e => e === "-noErrors"), noOutput: flags.some(e => e === "-noOutput"), noWhitelines: flags.some(e => e === "-noWhitelines"), noCredits: flags.some(e => e === "-noCredits")});
+    index.compile(args, {fullError: flags.some(e => e === "-fullErr"), generateJSON: flags.some(e => e === "-generateJSON"), noErrors: flags.some(e => e === "-noErrors"), noOutput: flags.some(e => e === "-noOutput"), noWhitelines: flags.some(e => e === "-noWhitelines"), noCredits: flags.some(e => e === "-noCredits"), respectJsonPath: flags.some(e => e === "-respectJsonPath")});
     break;
 
   case "watch":
-    index.watch(args, {fullError: flags.some(e => e === "-fullErr"), generateJSON: flags.some(e => e === "-generateJSON"), noErrors: !flags.some(e => e === "-err" || e === "-fullErr"), noInitial: flags.some(e => e === "-noInitial"), noOutput: flags.some(e => e === "-noOutput"), noWhitelines: flags.some(e => e === "-noWhitelines"), noCredits: flags.some(e => e === "-noCredits")});
+    index.watch(args, {fullError: flags.some(e => e === "-fullErr"), generateJSON: flags.some(e => e === "-generateJSON"), noErrors: !flags.some(e => e === "-err" || e === "-fullErr"), noInitial: flags.some(e => e === "-noInitial"), noOutput: flags.some(e => e === "-noOutput"), noWhitelines: flags.some(e => e === "-noWhitelines"), noCredits: flags.some(e => e === "-noCredits"), respectJsonPath: flags.some(e => e === "-respectJsonPath")});
     break;
 
   case "create":
